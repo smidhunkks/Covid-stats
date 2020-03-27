01ac7a2037292a8f642060854724c3cb0a3d9188
midhun <jmsmidhunkrishna@gmail.com>
2020-03-27 20:29:00 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var x=document.getElementById("bdy").addEventListener("load",apfh());
+/*var x=document.getElementById("butn").addEventListener("click",apfh());*/
+function apfh(){
+    var y=document.getElementById("res");
+    console.log("success");
+    fetch("https://covid-19-data.p.rapidapi.com/totals?format=undefined", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "7817005512msh32521a41050f8b3p1ba7bcjsn466b7a5211dd"
+	}
+})
+.then(response =>response.json())
+.then(data=>{
+    let output=`<h1 class="reshead">World Wide Stats</h1>`;
+    console.log(data);
+    y.innerHTML=output;
+})
+
+.catch(err => {
+	console.log(err);
+});
+}
